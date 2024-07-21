70e038d1007cff5717cd96a70a4b4e5cc7b85984
Sam Mina <samandmissy2010@gmail.com>
2024-07-21 11:30:35 -0400

    Pseudocoded the form JS

---
+++ my-blog/assets/js/form.js
@@ -0,0 +1,11 @@
+//Write the window event listener function so that the Document Object Model (DOM) can fully load before the script runs.
+// Select form element by ID.
+// Create an event listener to the form for user input. 
+//I am going to have to take the values of the input by their ids.
+//const username, const title, const content. All of these I will use the document.getElemntById.
+// I will use the ! and || to check to see if the fields are empty by making an if statement that will alert the user to please complete all fields.
+// Use JSON.parse to retreive from localStorage and blog posts. 
+// push new posts to array. 
+// Save blog array to localStorage using JSON.stringify since it has to be a string. 
+
+
