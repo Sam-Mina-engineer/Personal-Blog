104c49206d588982cd35c9834fa35bacbec1762a
Sam Mina <samandmissy2010@gmail.com>
2024-07-27 15:59:24 -0400

    Pseudocoded blog and logic js

---
+++ my-blog/assets/js/blog.js
@@ -0,0 +1,19 @@
+//Load DOM after HTML runs.
+
+//Use JSON.parse to get the blogPosts from localStorage. Use || [] to initialize it as an empty array if there are no posts to retrieve. 
+
+// Create a const variable to be a container for the blog posts. That way there is a reference to the container where the blog posts will show.
+
+// Use a forEach loop to go through each item in array and run the function given for each item. It will allow me
+// to go through each blog post and make an HTML element for it.
+
+// Need another create const variable to create a new div element so that there can be a new container for each blog post on the page.
+
+// Make sure to apply the CSS styles to the div element. And then populate the div element with the blog post. 
+
+// Append to container.
+
+// Sleect the element with ID back button and andd event listener for a click. window.location.href it back to index.html.
+
+// Make another one to toggle dark mode. 
+
---
+++ my-blog/assets/js/logic.js
@@ -0,0 +1,10 @@
+// Add event listner and wait for DOM content and make sure that HTML loads. 
+
+// Make const variable for positive dark mode and make Boolean statement for true. If it is true add dark mode class to body element. 
+
+// That way if dark mode is already on, it loads it with the page. 
+
+// Select element id toggle-mode and add an event listener for a click. That way we can switch back and forth on the dark mode.
+
+// Save the Boolean value of darkmode to localStorage.
+
